e32b85051c15b0a90d340f487f2d08d14fb61acd
Michal Škoula <michal@exitshop.cz>
2020-12-14 22:07:39 +0100

    ehm

--- modules/node.js
+++ modules/node.js
@@ -1,3 +1,4 @@
 // node modules without ESM support to be Browserified by Grunt
 window.ndarray = require('ndarray');
 window.createPlanner = require('l1-path-finder');
+window.colorScheme = require('color-scheme');
--- modules/tools.js
+++ modules/tools.js
@@ -2,6 +2,10 @@ export function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+export function randomInArray(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+
 export function collision(rect1, rect2) {
     return (
         rect1.x < rect2.x + rect2.width &&
